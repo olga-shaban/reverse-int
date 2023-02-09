a908cae3168729ec2788fe2fd2d74320e750dc73
Olga Shaban <schaban.olga@gmail.com>
2023-02-10 01:22:28 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+
 module.exports = function reverse (n) {
+    if (n<0) {
+        n = -n;
+    }
+    let result = 0;
+    while (n>0) {
+      result = result*10 + n % 10;
+      n = Math.floor(n / 10);
+      
+    }
   
+   return result;
 }
+
